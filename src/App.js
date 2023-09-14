9e026a037e3c2078d93a6ea91b3877f329187b81
Nikita <Nikitavishwakarma304@gmail.com>
2023-09-15 00:02:00 +0530

    Added Dark Mode in full page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 // import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 // import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({ msg: message, type: type });
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode has been enabled", "Success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "Success");
+    }
+  };
+
   return (
     <>
       {/* <Navbar title="Nikzz" abouttext="About Nikzz" /> */}
       {/* <Navbar /> */}
-      <Navbar title="Nikkzz" />
+      <Navbar title="Nikkzz" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container">
-        <TextForm heading="Enter the text to Analyse" />
+        <TextForm
+          showAlert={showAlert}
+          heading="Enter the text to Analyse"
+          mode={mode}
+        />
       </div>
+
       {/* <About /> */}
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,22 +6,26 @@ export default function TextForm(props) {
   const handleUpClick = () => {
     let newText = text.toUpperCase();
     setText(newText);
+    props.showAlert("Converted to Uppercase", "success");
   };
 
   const handleLowClick = () => {
     let newText = text.toLowerCase();
     setText(newText);
+    props.showAlert("Converted to Lowercase", "success");
   };
 
   const handleClearClick = () => {
     let newText = "";
     setText(newText);
+    props.showAlert("Text is Clear", "success");
   };
 
   const speak = () => {
     let msg = new SpeechSynthesisUtterance();
     msg.text = text;
     window.speechSynthesis.speak(msg);
+    props.showAlert("Spoken Successfully...", "success");
   };
 
   const handleOnChange = (event) => {
@@ -37,49 +41,48 @@ export default function TextForm(props) {
 
   const [Bstyle, setBstyle] = useState({
     fontWeight: "normal",
-  });
-  const toggleBStyle = () => {
-    if (text.fontWeight === "normal") {
-      setBstyle({
-        fontWeight: "normal",
-      });
-    } else {
-      setBstyle({
-        fontWeight: "bold",
-      });
-    }
-  };
-
-  const [Istyle, setIstyle] = useState({
     fontStyle: "normal",
+    textDecorationLine: "none",
   });
-  const toggleIStyle = () => {
-    if (text.fontStyle === "normal") {
-      setIstyle({
-        fontStyle: "italic",
-        color: "red",
-      });
-    } else {
-      setIstyle({
-        fontStyle: "italic",
-        color: "blue",
-      });
-    }
+
+  const toggleBStyle = () => {
+    setBstyle({
+      ...Bstyle,
+      fontWeight: "bold",
+    });
+    props.showAlert("Converted to Bold Letters", "success");
   };
 
-  const handleUClick = () => {
-    console.log();
+  const toggleIStyle = () => {
+    console.log(Bstyle);
+    setBstyle({
+      ...Bstyle,
+      fontStyle: "italic",
+    });
+    props.showAlert("Converted to Italic", "success");
+  };
+
+  const toggleUnderlineStyle = () => {
+    setBstyle({
+      ...Bstyle,
+      textDecorationLine: "Underline",
+    });
+    props.showAlert("underLine in text", "success");
   };
 
   const [text, setText] = useState("");
   // setText("New Text");
   return (
-    <>
+    <dic style={{ color: props.mode === "dark" ? "white" : "#042743" }}>
       <div className="container">
         <h1>{props.heading}</h1>
         <div className="mb-3">
           <textarea
-            style={{ ...Bstyle, ...Istyle }}
+            style={{
+              ...Bstyle,
+              backgroundColor: props.mode === "dark" ? "grey" : "white",
+              color: props.mode === "dark" ? "white" : "#042743",
+            }}
             className="form-control"
             value={text}
             onChange={handleOnChange}
@@ -104,15 +107,18 @@ export default function TextForm(props) {
           Bold
         </button>
 
-        <button onclick={toggleIStyle} className="btn btn-primary mx-1">
+        <button onClick={toggleIStyle} className="btn btn-primary mx-1">
           I
         </button>
-        <button className="btn btn-primary mx-1" onClick={handleUClick}>
+        <button onClick={toggleUnderlineStyle} className="btn btn-primary mx-1">
           U
         </button>
       </div>
 
-      <div className="container my-2">
+      <div
+        className="container my-2"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
         <h2>Your Summary Here...</h2>
         <p>
           {text.split(" ").length} words & {text.length} characters
@@ -121,6 +127,6 @@ export default function TextForm(props) {
         <h3>Preview</h3>
         <p>{text}</p>
       </div>
-    </>
+    </dic>
   );
 }
